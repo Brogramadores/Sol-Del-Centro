6a05556994507faa3a7812386a723656f6f742a1
Jose Esau Tovar Cruz <esautovarcz@gmail.com>
2025-07-15 12:46:57 -0600

    Estructura pequeña

---
+++ sol-del-centro/src/components/navbar.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import styles from '../styles/navbar.module.css';
+
+function navbar() {
+    return (
+
+        <h1>Hola :D</h1>
+
+    );
+}
